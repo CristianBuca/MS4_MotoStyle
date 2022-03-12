ebfb1b866e56cacbddb807fd4c39197e544a431c
CristianBuca <buca.cristian@hotmail.com>
2022-03-12 16:09:42 +0000

    Add(settings): home app and templates

--- moto_style/settings.py
+++ moto_style/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
+
 ]
 
 MIDDLEWARE = [
@@ -67,7 +69,10 @@ ROOT_URLCONF = 'moto_style.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
