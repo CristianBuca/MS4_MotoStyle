6f1cb307d9d804734e1d58ee0284557d10937bc1
CristianBuca <buca.cristian@hotmail.com>
2022-03-12 16:11:01 +0000

    Add(home-app): index template and url routes

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home')
+]
