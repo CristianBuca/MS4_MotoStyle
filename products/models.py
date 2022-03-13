8e043219206d530de27840af4c259939327fff83
CristianBuca <buca.cristian@hotmail.com>
2022-03-13 17:03:10 +0000

    Add(products-models): Product Class

--- products/models.py
+++ products/models.py
@@ -17,7 +17,7 @@ class Category(models.Model):
             Arguments: self (object): self
             Returns: Category name as string
         """
-        return self.name
+        return str(self.name)
 
     def get_friendly_name(self):
         """
@@ -26,3 +26,29 @@ class Category(models.Model):
             Returns: Category friendly name as string
         """
         return self.friendly_name
+
+
+class Product(models.Model):
+    """
+    Class defining the fields in Product model
+    """
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL
+    )
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True
+    )
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        """
+        Returns the category name string
+            Arguments: self (object): self
+            Returns: Category name as string
+        """
+        return str(self.name)
