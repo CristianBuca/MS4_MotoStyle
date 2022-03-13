e2470069d852cea231ebc7669557fbc92c61541c
CristianBuca <buca.cristian@hotmail.com>
2022-03-13 17:35:54 +0000

    Add(products-admin): Products and Cateogories
    Registered models in admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,13 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.contrib import admin
 
+# Internal:
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from .models import Product, Category
+
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Category)
