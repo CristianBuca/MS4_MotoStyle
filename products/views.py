873d5d406e23b3041e0866c78deb819762cb18bb
CristianBuca <buca.cristian@hotmail.com>
2022-03-13 23:33:43 +0000

    Start(products-views): products function
    displays all products in database

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
+# Imports
+# 3rd party:
 from django.shortcuts import render
+# -----------------------------------------------------------------------------
 
-# Create your views here.
+
+def products(request):
+    """
+    View to render the products page, handle sorting and search queries
+        Arguments: request (object): HTTP request object
+        Returns: render products page with context
+    """
+    return render(request, 'products/index.html')
