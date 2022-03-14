d1a07b00ea391f8cc543d0d35babf64f22268d8f
CristianBuca <buca.cristian@hotmail.com>
2022-03-14 22:52:29 +0000

    Add(products-views): products filter by category

--- products/views.py
+++ products/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
 # Internal:
-from .models import Product
+from .models import Product, Category
 # -----------------------------------------------------------------------------
 
 
@@ -16,8 +16,14 @@ def products(request):
     """
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -25,12 +31,13 @@ def products(request):
                 return redirect(reverse('products'))
 
             queries = (
-                Q(name__icontains=query) | Q(description__icontains=query)
+                Q(name__icontains=query) | Q(category__name__icontains=query)
             )
             products = products.filter(queries)
     context = {
         'products': products,
         'search_term': query,
+        'selected_categories': categories,
     }
     return render(request, 'products/products.html', context)
 
