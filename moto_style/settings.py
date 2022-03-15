218ec7bb7b5733c22f3e36cf9c13da2928dc9584
CristianBuca <buca.cristian@hotmail.com>
2022-03-15 18:13:40 +0000

    Add(cart): Setup cart contexts
    Cart details are available to all proj apps

--- moto_style/settings.py
+++ moto_style/settings.py
@@ -81,6 +81,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'cart.contexts.cart_contents',
             ],
         },
     },
@@ -165,6 +166,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 100
+STANDARD_DELIVERY_PERCENTAGE = 15
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
