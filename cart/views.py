5eba18c09b1bccd1d012863c931d6cae4e104fe1
CristianBuca <buca.cristian@hotmail.com>
2022-03-15 17:39:34 +0000

    Add(cart): View shopping cart functionality
    +Wired up cart app to project settings and urls

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,14 @@
+# Imports
+# 3rd party:
 from django.shortcuts import render
+# -----------------------------------------------------------------------------
 
-# Create your views here.
+
+def view_cart(request):
+    """
+    View to render the shopping cart page.
+        Arguments: request (object): HTTP request object
+        Returns: render shopping cart page
+    """
+
+    return render(request, 'cart/cart.html')
