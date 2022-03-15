f8a5297a99552f222861d97c8b14cc50a46e617b
CristianBuca <buca.cristian@hotmail.com>
2022-03-15 20:33:41 +0000

    Add(cart-views): Add item and quantity to cart

--- cart/views.py
+++ cart/views.py
@@ -17,10 +17,22 @@ def view_cart(request):
 def add_to_cart(request, product_id):
     """
     View to add the product to the shopping cart stored in session cookies.
-        Arguments: 
+        Arguments:
             request (object): HTTP request object
             product_id: ID of product from the form
         Returns:
             redirect_url: Redirect back to product_detail view
     """
-    return redirect()
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if product_id in list(cart.keys()):
+        cart[product_id] += quantity
+    else:
+        cart[product_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
