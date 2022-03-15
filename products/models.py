734bd78a2b98d0652000444f2564418418d1b85e
CristianBuca <buca.cristian@hotmail.com>
2022-03-15 23:25:21 +0000

    Add(products-models): size field to products
    Migrated new models to db

--- products/models.py
+++ products/models.py
@@ -47,6 +47,7 @@ class Product(models.Model):
     )
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
 
     def __str__(self):
         """
