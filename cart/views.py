8c17001cc5d5e2d936b16e48286eb90a00a789d2
CristianBuca <buca.cristian@hotmail.com>
2022-03-16 18:44:59 +0000

    Start(cart-views): Adjust cart view

--- cart/views.py
+++ cart/views.py
@@ -46,5 +46,32 @@ def add_to_cart(request, item_id):
             cart[item_id] = quantity
 
     request.session['cart'] = cart
-    print(request.session['cart'])
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """
+    View to adjust the quantity of the product in the shopping cart stored in session cookies.
+        Arguments:
+            request (object): HTTP request object
+            item_id: ID of product from the form
+        Returns:
+            redirect_url: Redirect back to product_detail view
+    """
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+
+    if size:
+        if 
+    else:
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+        else:
+            cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    return redirect()
