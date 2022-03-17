b2c560f9f7ad075f753e2841f54694ad5ac0b8e4
CristianBuca <buca.cristian@hotmail.com>
2022-03-17 22:22:42 +0000

    Add(proj-settings): config for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn moto_style.wsgi:application
--- moto_style/settings.py
+++ moto_style/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ms4-motostyle.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
