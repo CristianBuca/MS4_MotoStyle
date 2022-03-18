90b69d7d10d94550ced88bd03de9a0161dc98e69
CristianBuca <buca.cristian@hotmail.com>
2022-03-18 17:58:14 +0000

    Add(checkout-views): View for user to checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,28 @@
 # Imports
 # 3rd party:
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+# Internal:
+from .forms import OrderForm
 # -----------------------------------------------------------------------------
 
 
 def checkout(request):
-    
+    """
+    Defines the checkout view
+        Arguments: request (object): The checkout request
+        Returns: render of checkout page
+    """
 
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, "Your shopping cart is empty")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
