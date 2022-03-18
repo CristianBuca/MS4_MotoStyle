3fb87fac27ade46854f75d5ed39eecd07dc105f3
CristianBuca <buca.cristian@hotmail.com>
2022-03-18 22:49:36 +0000

    Add(checkout-views): stripe payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -1,15 +1,18 @@
 # Imports
 # 3rd party:
-import os
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
+import stripe
 # Internal:
 from .forms import OrderForm
+from cart.contexts import cart_contents
 # -----------------------------------------------------------------------------
 
 
 def checkout(request):
-    stripe_public_key = os.environ.get('STRIPE_PUBLIC_KEY')
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     """
     Defines the checkout view
         Arguments: request (object): The checkout request
@@ -21,12 +24,25 @@ def checkout(request):
         messages.error(request, "Your shopping cart is empty")
         return redirect(reverse('products'))
 
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key missing.')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
-        'client_secret': 'client_secret',
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
