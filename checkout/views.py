4acb534c6acf93550cd90abe5082380eaa2a8f34
CristianBuca <buca.cristian@hotmail.com>
2022-03-18 22:26:39 +0000

    Script(checkout): Add stripe element 2 checkout pg

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,6 @@
 # Imports
 # 3rd party:
+import os
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 # Internal:
@@ -8,6 +9,7 @@ from .forms import OrderForm
 
 
 def checkout(request):
+    stripe_public_key = os.environ.get('STRIPE_PUBLIC_KEY')
     """
     Defines the checkout view
         Arguments: request (object): The checkout request
@@ -23,6 +25,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': 'client_secret',
     }
 
     return render(request, template, context)
