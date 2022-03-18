61583e540152ad05e9a6143bfe7b6f5eb22c0fea
CristianBuca <buca.cristian@hotmail.com>
2022-03-18 14:38:51 +0000

    Start(checkout-admin): Admin for the checkout app

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,9 @@
+# Imports
+# 3rd party:
 from django.contrib import admin
+# Internal:
+from .models import Order, OrderLineItem
+# -----------------------------------------------------------------------------
 
-# Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
