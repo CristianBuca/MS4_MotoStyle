e4bfb951563021307ff3792f2e1607de57b4608e
CristianBuca <buca.cristian@hotmail.com>
2022-03-19 22:49:22 +0000

    Start(profiles-forms): form for the User profile

---
+++ profiles/forms.py
@@ -0,0 +1,49 @@
+# Imports
+# 3rd party:
+from django import forms
+# Internal
+from .models import UserProfile
+# -----------------------------------------------------------------------------
+
+
+class UserProfileForm(forms.ModelForm):
+    """
+    Class defining the fields and methods used on the Order Form
+    """
+    class UserProfile:
+        """
+        Defines the Meta fields on the Order Form
+        """
+        model = UserProfile
+        exclude = ('user',)
+
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+            Arguments: self (object): The object using the method
+            Returns: N/A
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'address_line1': 'Address Line 1',
+            'address_line2': 'Address Line 2',
+            'town_or_city': 'Town or City',
+            'postcode': 'Postal Code',
+            'area': 'Area',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+                self.fields[field].label = False
