442583a6cc015e8e5dbe6419ee43a7712cc67c39
CristianBuca <buca.cristian@hotmail.com>
2022-03-19 19:59:58 +0000

    Start(profiles-views): profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,16 @@
+# Imports
+# 3rd Party
 from django.shortcuts import render
+# -----------------------------------------------------------------------------
 
-# Create your views here.
+
+def profile(request):
+    """
+    Displays the user's profile
+        Arguments: request (object): The request object
+        Return: Render user's profile page
+    """
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
