02325494a22c678cda7fa3c2c38f5aa87ff7da5a
CristianBuca <buca.cristian@hotmail.com>
2022-03-19 19:32:49 +0000

    Add(profiles-models): UserProfile class
    Defines the delivery details fields in user profile

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,34 @@
+# Imports
+# 3rd party:
 from django.db import models
+from django.contrib.auth.models import User
+from django_countries.fields import CountryField
+# -----------------------------------------------------------------------------
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    """
+    Class to give the user the ability to store default delivery information
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_full_name = models.CharField(max_length=50, null=True, blank=True)
+    default_email = models.EmailField(max_length=254, null=True, blank=True)
+    default_phone_number = models.CharField(
+        max_length=20, null=True, blank=True
+    )
+    default_address_line1 = models.CharField(
+        max_length=80, null=True, blank=True
+    )
+    default_address_line2 = models.CharField(
+        max_length=80, null=True, blank=True
+    )
+    default_postcode = models.CharField(
+        max_length=20, null=True, blank=True
+    )
+    default_town_or_city = models.CharField(
+        max_length=40, null=True, blank=True
+    )
+    default_area = models.CharField(max_length=80, null=True, blank=True)
+    default_country = CountryField(
+        blank_label='Country *', null=True, blank=True
+    )
