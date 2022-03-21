79e857d9f0c2f7e1d1c954f1a8668159a935e5e8
CristianBuca <buca.cristian@hotmail.com>
2022-03-21 20:29:50 +0000

    Add(products-forms): Product Form

--- products/forms.py
+++ products/forms.py
@@ -7,5 +7,27 @@ from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
+    """
+    Class defining the form for Product management
+    """
+    class Meta:
+        model = Product
+        fields = '__all__'
 
-    
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize method for the form
+            Arguments:
+                self (object): The object initialized
+                *args: other arguments
+                **kwargs: keyword arguments
+            Returns:
+                N/A
+        """
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
