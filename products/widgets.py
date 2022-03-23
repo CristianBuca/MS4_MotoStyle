0a8196957ae3bc12fdb49bf212e123a6fa80836e
CristianBuca <buca.cristian@hotmail.com>
2022-03-23 23:16:10 +0000

    Start(products): widgets with clearable file input

---
+++ products/widgets.py
@@ -0,0 +1,8 @@
+# Imports
+# 3rd Party
+from django.forms.widgets import ClearableFileInput
+from django.utils.translation import gettext_lazy as _
+# -----------------------------------------------------------------------------
+
+class CustomClearableFileInput(ClearableFileInput):
+    
