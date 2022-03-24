6c632bb027982cd224819ade90db0708d4c9dbdc
CristianBuca <buca.cristian@hotmail.com>
2022-03-24 22:37:25 +0000

    Start(products): Views unit testing

--- products/test_views.py
+++ products/test_views.py
@@ -3,3 +3,10 @@
 from django.test import TestCase
 # -----------------------------------------------------------------------------
 # Create your tests here.
+
+class TestProductsViews(TestCase):
+    """
+    Defines the views unit tests for the products app
+    """
+    def test_get_products(self):
+        
