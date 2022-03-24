d587a53a804883e2e59431b3b4c4c189943b24ef
CristianBuca <buca.cristian@hotmail.com>
2022-03-24 22:32:34 +0000

    Add(home-test): Unit tests for views in home app

---
+++ home/test_views.py
@@ -0,0 +1,18 @@
+# Imports
+# 3rd party:
+from django.test import TestCase
+# -----------------------------------------------------------------------------
+
+
+class TestHomeViews(TestCase):
+    """
+    Class defining the views tests for the home app
+    """
+    def test_home_page(self):
+        """
+        Test if the home page is accessible with a status code of 200
+        Test if the the template rendered is 'index.html'
+        """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
