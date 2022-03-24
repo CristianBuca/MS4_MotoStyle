9ca7c52fca86b3af3e91e9cbb1a7389c0222af2b
CristianBuca <buca.cristian@hotmail.com>
2022-03-24 22:53:07 +0000

    Add(products-tests_views): test_get_products

--- products/test_views.py
+++ products/test_views.py
@@ -2,11 +2,19 @@
 # 3rd party:
 from django.test import TestCase
 # -----------------------------------------------------------------------------
-# Create your tests here.
+
 
 class TestProductsViews(TestCase):
     """
     Defines the views unit tests for the products app
     """
     def test_get_products(self):
-        
+        """
+        Tests if products page is accessible with status code of 200
+        Tests if products view renders products.html
+        """
+        response = self.client.get('/products/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/products.html')
+
+    
