b23ba862406ca492791f5acdd8b1d45d2da32c91
CristianBuca <buca.cristian@hotmail.com>
2022-03-25 21:57:22 +0000

    Add(wishlist-urls): remove from wishlist path

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -9,4 +9,8 @@ from . import views
 urlpatterns = [
     path('', views.view_wishlist, name='view_wishlist'),
     path('add/<product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
+    path(
+        'remove/<product_id>/', views.remove_from_wishlist,
+        name='remove_from_wishlist'
+    ),
 ]
