14c5ed3a9ad7e9b0dfb458c747f415d9d9b22b13
CristianBuca <buca.cristian@hotmail.com>
2022-03-25 15:38:58 +0000

    Add(wishlist-views): view_wishlist function

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# Imports
+# 3rd party:
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404
+    )
+from django.contrib import messages
+# Internal
+from products.models import Product
+# -----------------------------------------------------------------------------
 
-# Create your views here.
+
+def view_wishlist(request):
+    """
+    View to display the user's wishlist
+        Arguments: request (object): The request object
+        Returns: Render the user's wishlist page
+    """
+
+    return render(request, 'wishlist/wishlist.html')
