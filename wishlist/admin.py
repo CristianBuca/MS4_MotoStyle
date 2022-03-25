5d69aa77b9c17300598b970521ab201b4ac0b1ce
CristianBuca <buca.cristian@hotmail.com>
2022-03-25 20:24:34 +0000

    Add(wishlist-admin): reg Wishlist model for admin

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Wishlist
+
+
+class WishlistAdmin(admin.ModelAdmin):
+    """
+    Admin class for the Wishlist model.
+    """
+    list_display = (
+        'owner',
+    )
+    search_fields = (
+        'owner',
+    )
+    list_filter = (
+        'owner',
+    )
+    list_per_page = 20
+
+
+admin.site.register(Wishlist, WishlistAdmin)
