ada958db7e3457b05c6cf7865affcba5bd0f707d
CristianBuca <buca.cristian@hotmail.com>
2022-03-25 17:03:41 +0000

    Add(wishlist-models): The wishlist model

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,24 @@
+# Imports
+# 3rd party:
 from django.db import models
+from django.contrib.auth.models import User
+# Internal
+from products.models import Product
+# -----------------------------------------------------------------------------
 
-# Create your models here.
+
+class Wishlist(models.Model):
+    """
+    Defines the Favorites model
+    """
+
+    products = models.ManyToManyField(Product, blank=True)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """
+        Returns the name of the wishlist as a string
+            Arguments: self (object): The wishlist object
+            Return: The wishlist's name as string
+        """
+        return f"{self.owner}'s Wishlist"
