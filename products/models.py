a5ba61a39b072bf36fb67cb5e39e1854a0f24303
CristianBuca <buca.cristian@hotmail.com>
2022-03-27 23:52:46 +0100

    Add(products-models): new_rating field to Product

--- products/models.py
+++ products/models.py
@@ -45,6 +45,9 @@ class Product(models.Model):
     rating = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True
     )
+    new_rating = models.DecimalField(
+        max_digits=6, decimal_places=2, default=0, null=True, blank=True
+    )
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
