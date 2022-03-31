5aca2b2ca5d16eec66660d275a72c590c90148c2
CristianBuca <buca.cristian@hotmail.com>
2022-03-31 10:40:14 +0100

    Script(home-js): Code changed to fit proj purpose

--- home/static/home/js/slider.js
+++ home/static/home/js/slider.js
@@ -1,20 +1,16 @@
 // Control for the card slider on index page
-// Credit to Paul Phillips: https://codepen.io/pau1phi11ips/pen/rKyGzq
+// Original code byPaul Phillips: https://codepen.io/pau1phi11ips/pen/rKyGzq
+// Modified to fit this project's purpose
 
-$('.toggle-all').on('click', function() {
-    $('#infinite_carousel .row').toggleClass('flex-nowrap');
-    $('#infinite_carousel .control').toggleClass('d-none');
-});
-
-$('#infinite_carousel .fa-chevron-right').on('click', () => {
-    let $infinite_carousel_row = $('#infinite_carousel .row');
+$('#infinite-carousel .control-right').on('click', () => {
+    let $infinite_carousel_row = $('#infinite-carousel .row');
     let $col = $infinite_carousel_row.find('.col-12:first');
     $infinite_carousel_row.append($col[0].outerHTML);
     $col.remove();
     });
 
-$('#infinite_carousel .fa-chevron-left').on('click', () => {
-    let $infinite_carousel_row = $('#infinite_carousel .row');
+$('#infinite-carousel .control-left').on('click', () => {
+    let $infinite_carousel_row = $('#infinite-carousel .row');
     let $col = $infinite_carousel_row.find('.col-12:last');
     $infinite_carousel_row.prepend($col[0].outerHTML);
     $col.remove();
