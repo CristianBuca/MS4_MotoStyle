e03fbf4f4d5ae86e6879addb0cd3c041929df44e
CristianBuca <buca.cristian@hotmail.com>
2022-04-07 02:17:40 +0100

    Start(wishlist-test_views): Wishlist views tests

---
+++ wishlist/test_views.py
@@ -0,0 +1,9 @@
+# Imports
+# 3rd Party:
+from django.test import TestCase
+# -----------------------------------------------------------------------------
+
+class TestWishlistViews(TestCase):
+    """
+    Defines the views unit tests for the wishlist app
+    """
