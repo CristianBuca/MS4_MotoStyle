35895f6414dcf2dda78cdf9e7cb40b820691c809
CristianBuca <buca.cristian@hotmail.com>
2022-04-07 19:18:57 +0100

    Add(cart-test_views): test_add_to_cart_no_size

--- cart/test_views.py
+++ cart/test_views.py
@@ -1,6 +1,9 @@
 # Imports
 # 3rd party:
 from django.test import TestCase
+from django.contrib.messages import get_messages
+# Internal
+from products.models import Product
 # -----------------------------------------------------------------------------
 
 
@@ -16,3 +19,24 @@ class TestCartViews(TestCase):
         response = self.client.get('/cart/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed('cart/cart.html')
+
+    def test_add_to_cart_no_size(self):
+        """
+        Tests if user is able to add a product with no size to the cart
+        Tests if toast displays correct message
+        """
+        product = Product.objects.create(
+            name='Test Product',
+            price='123.45',
+            description='Test Product Description',
+        )
+        response = self.client.post(
+            f'/cart/add/{product.id}/',
+            {'quantity': 1, 'redirect_url': 'view_cart'}
+        )
+        cart = self.client.session['cart']
+        self.assertEqual(cart[str(product.id)], 1)
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(
+            str(messages[0]), f'Added {product.name} to your cart'
+        )
