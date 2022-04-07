ec4cf19dc8844a88dbcbe880659dc98135a5e889
CristianBuca <buca.cristian@hotmail.com>
2022-04-07 01:23:38 +0100

    Add(profiles-test_views): test_get_profile

--- profiles/test_views.py
+++ profiles/test_views.py
@@ -1,6 +1,23 @@
 # Imports
 # 3rd Party:
 from django.test import TestCase
+from django.contrib.auth.models import User
 # -----------------------------------------------------------------------------
 
 
+class TestProfileViews(TestCase):
+    """
+    Defines the views unit tests for the profiles app
+    """
+    def test_get_profile(self):
+        """
+        Tests if profile page is accessible to the user with status 200
+        Tests if view renders the profile.html template
+        """
+        user = User.objects.create_user(
+            username='unit_test_user', password='unit_test_pass'
+        )
+        self.client.login(username='unit_test_user', password='unit_test_pass')
+        response = self.client.get('/profile/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profiles/profile.html')
