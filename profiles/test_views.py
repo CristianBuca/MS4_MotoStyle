f3bc79ac274f7c4199b05a5f2247816251fd1d35
CristianBuca <buca.cristian@hotmail.com>
2022-04-07 01:33:49 +0100

    Add(profiles-test_views): test_post_profile

--- profiles/test_views.py
+++ profiles/test_views.py
@@ -2,6 +2,7 @@
 # 3rd Party:
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.contrib.messages import get_messages
 # -----------------------------------------------------------------------------
 
 
@@ -21,3 +22,17 @@ class TestProfileViews(TestCase):
         response = self.client.get('/profile/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'profiles/profile.html')
+
+    def test_post_profile(self):
+        """
+        Tests if user can edit their delivery information
+        Tests if correct message is displayed in toast
+        """
+        user = User.objects.create_user(
+            username='unit_test_user', password='unit_test_pass'
+        )
+        self.client.login(username='unit_test_user', password='unit_test_pass')
+        response = self.client.post('/profile/')
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(str(messages[0]), 'Profile updated successfully')
