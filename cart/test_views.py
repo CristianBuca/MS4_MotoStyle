4d74b6ca1e0f00f4a0d43c58d96fb9b68726aae7
CristianBuca <buca.cristian@hotmail.com>
2022-04-07 19:31:13 +0100

    Add(cart-test_views): test_add_to_cart_has_size

--- cart/test_views.py
+++ cart/test_views.py
@@ -40,3 +40,22 @@ class TestCartViews(TestCase):
         self.assertEqual(
             str(messages[0]), f'Added {product.name} to your cart'
         )
+
+    def test_add_to_cart_has_size(self):
+        """
+        Tests if user is able to add a product with size to the cart
+        Tests if toast displays correct message
+        """
+        product = Product.objects.create(
+            name='Test Product',
+            price='123.45',
+            description='Test Product Description',
+        )
+        response = self.client.post(
+            f'/cart/add/{product.id}/',
+            {'quantity': 1, 'redirect_url': 'view_cart', 'product_size': 'XS'}
+        )
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(
+            str(messages[0]), f'Added size XS {product.name} to your cart'
+        )
