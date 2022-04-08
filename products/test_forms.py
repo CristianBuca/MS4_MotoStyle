e002194d6f16745f28e0399b691c37adc3d4f61c
CristianBuca <buca.cristian@hotmail.com>
2022-04-09 00:18:58 +0100

    Add(products-test_forms): test_product_form_no_nam

--- products/test_forms.py
+++ products/test_forms.py
@@ -1,6 +1,8 @@
 # Imports
 # 3rd party:
 from django.test import TestCase
+# Internal
+from .forms import ProductForm
 # -----------------------------------------------------------------------------
 
 
@@ -11,5 +13,13 @@ class TestProductsForms(TestCase):
 
     def test_product_form_no_name(self):
         """
-        
+        Tests if correct error is displayed to the user when
+        trying to submit form with empty name field
+        Tests if form fails validation
         """
+        form = ProductForm({'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('name', form.errors.keys())
+        self.assertEqual(
+            form.errors['name'][0], 'This field is required.'
+        )
