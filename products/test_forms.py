8dacdffcaf7ba8a204a027a4ec5436a761fbb860
CristianBuca <buca.cristian@hotmail.com>
2022-04-09 00:20:45 +0100

    Add(products-test_forms): test_prod_form_no_price

--- products/test_forms.py
+++ products/test_forms.py
@@ -23,3 +23,19 @@ class TestProductsForms(TestCase):
         self.assertEqual(
             form.errors['name'][0], 'This field is required.'
         )
+
+    def test_product_form_no_price(self):
+        """
+        Tests if correct error is displayed to the user when
+        trying to submit form with empty name field
+        Tests if form fails validation
+        """
+        form = ProductForm({
+            'name': 'Unit Test Product',
+            'price': ''
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('price', form.errors.keys())
+        self.assertEqual(
+            form.errors['price'][0], 'This field is required.'
+        )
