6cb281651233667a01377eff4f56169805052ce7
CristianBuca <buca.cristian@hotmail.com>
2022-04-09 00:36:36 +0100

    Add(profiles-test_models) test_profile_str

--- profiles/test_models.py
+++ profiles/test_models.py
@@ -1,6 +1,9 @@
 # Imports
 # 3rd party:
 from django.test import TestCase
+from django.contrib.auth.models import User
+# Internal
+from .models import UserProfile
 # -----------------------------------------------------------------------------
 
 
@@ -13,4 +16,10 @@ class TestProfileModels(TestCase):
         """
         Tests if UserProfile model str method returns correct string
         """
-        
+        user = User.objects.create_user(
+            username='unit_test',
+            password='test_pass',
+            email='test@unittest.com',
+        )
+        user_profile = UserProfile.objects.get(user=user)
+        self.assertEqual(str(user_profile), user_profile.user.username)
