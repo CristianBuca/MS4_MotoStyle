df341cba3a9c4c630d94df8d74ffd7f5ee1b32b0
CristianBuca <buca.cristian@hotmail.com>
2022-04-08 23:14:40 +0100

    Start(checkout-test_models): Models unit tests

---
+++ checkout/test_models.py
@@ -0,0 +1,11 @@
+# Imports
+# 3rd party:
+from django.test import TestCase
+# Internal
+from .models import Order
+# -----------------------------------------------------------------------------
+
+class TestCheckoutModels(TestCase):
+    """
+    Defines the models unit test for the checkout app
+    """
