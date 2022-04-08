dc49952c546d253d8fb4bf615b5d4309a3167379
CristianBuca <buca.cristian@hotmail.com>
2022-04-08 23:25:57 +0100

    Add(checkout-test_models): test_order_model

--- checkout/test_models.py
+++ checkout/test_models.py
@@ -5,7 +5,22 @@ from django.test import TestCase
 from .models import Order
 # -----------------------------------------------------------------------------
 
+
 class TestCheckoutModels(TestCase):
     """
     Defines the models unit test for the checkout app
     """
+    def test_order_model(self):
+        """
+        Tests if order model returns correct string
+        """
+        Order.objects.create(
+            full_name='test user',
+            email='test@unittest.com',
+            phone_number='1234567890',
+            address_line1='test address line',
+            town_or_city='test city',
+            country='US',
+        )
+        order = Order.objects.get(email='test@unittest.com')
+        self.assertEqual(str(order), order.order_number)
