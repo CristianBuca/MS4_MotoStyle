6f23edd1a63a751ae54e2917b9eb758488abfc46
CristianBuca <buca.cristian@hotmail.com>
2022-04-08 22:21:32 +0100

    Start(checkout-test_forms): Forms unit tests

---
+++ checkout/test_forms.py
@@ -0,0 +1,9 @@
+# Imports
+# 3rd party:
+from django.test import TestCase
+# -----------------------------------------------------------------------------
+
+def test_order_form(self):
+    """
+    
+    """
