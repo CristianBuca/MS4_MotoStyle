260dfce9d941c2d05557107b98ba66a1bda63e71
CristianBuca <buca.cristian@hotmail.com>
2022-04-08 22:39:35 +0100

    Add(checkout-test_forms): test_order_form_no_email

--- checkout/test_forms.py
+++ checkout/test_forms.py
@@ -10,6 +10,7 @@ class TestsCheckoutForms(TestCase):
     """
     Defines the forms unit tests for the checkout app
     """
+
     def test_order_form_no_name(self):
         """
         Tests if correct error is displayed to the user when
@@ -21,4 +22,20 @@ class TestsCheckoutForms(TestCase):
         self.assertIn('full_name', form.errors.keys())
         self.assertEqual(
             form.errors['full_name'][0], 'This field is required.'
+        )
+
+    def test_order_form_no_email(self):
+        """
+        Tests if correct error is displayed to the user when
+        trying to submit form with empty name field
+        Tests if form fails validation
+        """
+        form = OrderForm({
+            'full_name': 'test user',
+            'email': '',
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('email', form.errors.keys())
+        self.assertEqual(
+            form.errors['email'][0], 'This field is required.'
         )
