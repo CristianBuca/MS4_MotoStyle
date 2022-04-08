4370ed2de8683a65f5c0acb513c009316d219b61
CristianBuca <buca.cristian@hotmail.com>
2022-04-08 22:36:51 +0100

    Add(checkout-test_forms): test_order_form_no_name

--- checkout/test_forms.py
+++ checkout/test_forms.py
@@ -1,9 +1,24 @@
 # Imports
 # 3rd party:
 from django.test import TestCase
+# Internal:
+from .forms import OrderForm
 # -----------------------------------------------------------------------------
 
-def test_order_form(self):
+
+class TestsCheckoutForms(TestCase):
     """
-    
+    Defines the forms unit tests for the checkout app
     """
+    def test_order_form_no_name(self):
+        """
+        Tests if correct error is displayed to the user when
+        trying to submit form with empty name field
+        Tests if form fails validation
+        """
+        form = OrderForm({'full_name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('full_name', form.errors.keys())
+        self.assertEqual(
+            form.errors['full_name'][0], 'This field is required.'
+        )
