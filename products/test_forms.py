e65771596ecf19550466e3eaf84174c7133f13db
CristianBuca <buca.cristian@hotmail.com>
2022-04-09 00:15:00 +0100

    Start(products-test_forms): Forms unit tests

---
+++ products/test_forms.py
@@ -0,0 +1,15 @@
+# Imports
+# 3rd party:
+from django.test import TestCase
+# -----------------------------------------------------------------------------
+
+
+class TestProductsForms(TestCase):
+    """
+    Defines the forms unit tests for the products app
+    """
+
+    def test_product_form_no_name(self):
+        """
+        
+        """
