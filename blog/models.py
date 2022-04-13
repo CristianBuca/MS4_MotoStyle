8ff04482869e177b0e62d19e8de611fd1f41f7b4
CristianBuca <buca.cristian@hotmail.com>
2022-04-13 01:43:09 +0100

    Add(blog-models): BlogPost model

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 # Imports
 # 3rd party:
 from django.db import models
+from django.contrib.auth.models import User
 # -----------------------------------------------------------------------------
 
 
@@ -9,4 +10,24 @@ class BlogPost(models.Model):
     Class defining the fields in BlogPost model
     """
 
-    
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=254)
+    content = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    second_image_url = models.URLField(max_length=1024, null=True, blank=True)
+    second_image = models.ImageField(null=True, blank=True)
+    third_image_url = models.URLField(max_length=1024, null=True, blank=True)
+    third_image = models.ImageField(null=True, blank=True)
+    fourth_image_url = models.URLField(max_length=1024, null=True, blank=True)
+    fourth_image = models.ImageField(null=True, blank=True)
+    fifth_image_url = models.URLField(max_length=1024, null=True, blank=True)
+    fifth_image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        """
+        Returns the blogpost programatic title string
+            Arguments: self (object): self
+            Returns: BlogPost programatic title as string
+        """
+        return str(self.title)
