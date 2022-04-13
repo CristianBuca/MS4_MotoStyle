a2f41b540e80036a1a43445fa3061ea36ec607b5
CristianBuca <buca.cristian@hotmail.com>
2022-04-13 15:52:07 +0100

    Add(blog-views): blog_post display for a blog post

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 # Imports
 # 3rd party:
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # Internal:
 from .models import BlogPost
 # -----------------------------------------------------------------------------
@@ -9,8 +9,8 @@ from .models import BlogPost
 def blog(request):
     """
     View to render the blog page
-    Arguments: request (object): The Http request
-    Returns: render blog page with context
+        Arguments: request (object): The Http request
+        Returns: render blog page with context
     """
 
     blog = BlogPost.objects.all()
@@ -19,3 +19,20 @@ def blog(request):
     }
 
     return render(request, 'blog/blog.html', context)
+
+
+def blog_post(request, blog_post_id):
+    """
+    View to render blog posts
+        Arguments:
+            request (object): The Http request
+            blog_post_id: ID of the blog post being viewed
+        Returns: render blog post display page with context
+    """
+    blog_post = get_object_or_404(BlogPost, pk=blog_post_id)
+
+    context = {
+        'blog_post': blog_post,
+    }
+
+    return render(request, 'blog/blog_post.html', context)
