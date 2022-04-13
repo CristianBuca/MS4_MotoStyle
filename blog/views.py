1675624fc36be7d9c980ce812cc8351356c5f2fd
CristianBuca <buca.cristian@hotmail.com>
2022-04-13 15:44:54 +0100

    Add(blog-views): blog view to display all posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
+# Imports
+# 3rd party:
 from django.shortcuts import render
+# Internal:
+from .models import BlogPost
+# -----------------------------------------------------------------------------
 
-# Create your views here.
+
+def blog(request):
+    """
+    View to render the blog page
+    Arguments: request (object): The Http request
+    Returns: render blog page with context
+    """
+
+    blog = BlogPost.objects.all()
+    context = {
+        'blog': blog,
+    }
+
+    return render(request, 'blog/blog.html', context)
