77c492ed7b0c1ef76b44d0d628371e3af3917029
CristianBuca <buca.cristian@hotmail.com>
2022-04-14 22:10:40 +0100

    Add(blog-admin): BlogAdmin model and register

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,23 @@
+# Imports
+# 3rd party:
 from django.contrib import admin
+# Internal:
+from .models import BlogPost
+# -----------------------------------------------------------------------------
 
-# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    """
+    Class defining the BlogPost model in admin page
+    """
+    list_display = (
+        'id', 'title', 'created_at', 'image', 'second_image', 'third_image',
+        'fourth_image', 'fifth_image',
+    )
+
+    ordering = ('owner', 'created_at')
+
+    search_fields = ('title',)
+
+
+admin.site.register(BlogPost, BlogAdmin)
