d128fe9396a7073bba88091e0bdc048b25e3f769
CristianBuca <buca.cristian@hotmail.com>
2022-04-16 00:29:50 +0100

    Add(blog-admin): CommentAdmin and register

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,7 @@
 # 3rd party:
 from django.contrib import admin
 # Internal:
-from .models import BlogPost
+from .models import BlogPost, Comment
 # -----------------------------------------------------------------------------
 
 
@@ -11,13 +11,25 @@ class BlogAdmin(admin.ModelAdmin):
     Class defining the BlogPost model in admin page
     """
     list_display = (
-        'id', 'title', 'created_at', 'image', 'second_image', 'third_image',
-        'fourth_image', 'fifth_image',
+        'id', 'owner', 'title', 'created_at', 'image', 'second_image',
+        'third_image', 'fourth_image', 'fifth_image',
     )
-
     ordering = ('owner', 'created_at')
+    search_fields = ('title', )
+    list_filter = ('owner', )
 
-    search_fields = ('title',)
+
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Admin class for the Comment model.
+    """
+    list_display = (
+        'posted_by', 'blog_article', 'posted_at'
+    )
+    ordering = ('posted_by', 'posted_at')
+    search_fields = ('posted_by', 'blog_article')
+    list_filter = ('posted_by', )
 
 
 admin.site.register(BlogPost, BlogAdmin)
+admin.site.register(Comment, CommentAdmin)
--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,6 +1,9 @@
+# Imports
+# 3rd party:
 from django.contrib import admin
-
+# Internal:
 from .models import Wishlist
+# -----------------------------------------------------------------------------
 
 
 class WishlistAdmin(admin.ModelAdmin):
