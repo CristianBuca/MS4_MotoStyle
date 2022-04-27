263d42b7d0364ec8bc893bbac92848653dafd52f
CristianBuca <buca.cristian@hotmail.com>
2022-04-27 15:19:54 +0100

    Add(blog-test_views): test_get_blog_post

--- blog/test_views.py
+++ blog/test_views.py
@@ -1,6 +1,7 @@
 # Imports
 # 3rd party:
 from django.test import TestCase
+from django.contrib.auth.models import User
 # Internal
 from .models import BlogPost, Comment
 # -----------------------------------------------------------------------------
@@ -18,3 +19,23 @@ class TestBlogViews(TestCase):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'blog/blog.html')
+
+    def test_get_blog_post(self):
+        """
+        Tests if blog_post page is accessible with status code of 200
+        Tests if blog_post view renders blog_post.html
+        """
+        user = User.objects.create_user(
+            username='unit_test_user', password='unit_test_pass'
+        )
+        self.client.login(
+            username='unit_test_user', password='unit_test_pass'
+        )
+        blog_post = BlogPost.objects.create(
+            title='Test Blog Post',
+            content='Test Blog Post content',
+            owner=user,
+        )
+        response = self.client.get(f'/blog/{blog_post.id}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/blog_post.html')
