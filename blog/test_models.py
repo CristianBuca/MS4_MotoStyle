948b0932d21eda75f3acb737ad3cb59d8e455b72
CristianBuca <buca.cristian@hotmail.com>
2022-04-27 15:02:40 +0100

    Add(blog-test_models): test_blogpost_str

--- blog/test_models.py
+++ blog/test_models.py
@@ -1,9 +1,11 @@
 # Imports
 # 3rd party:
 from django.test import TestCase
+from django.contrib.auth.models import User
 # Internal
 from .models import BlogPost, Comment
 # -----------------------------------------------------------------------------
+
 
 class TestBlogModels(TestCase):
     """
@@ -14,3 +16,15 @@ class TestBlogModels(TestCase):
         """
         Tests if BlogPost model str method returns correct string
         """
+        user = User.objects.create_user(
+            username='unit_test_user', password='unit_test_pass'
+        )
+        self.client.login(
+            username='unit_test_user', password='unit_test_pass'
+        )
+        blog_post = BlogPost.objects.create(
+            title='Test Blog Post',
+            content='Test Blog Post content',
+            owner=user,
+        )
+        self.assertEqual(blog_post.__str__(), blog_post.title)
