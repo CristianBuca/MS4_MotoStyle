a9535e2b462a1e99142e662c05112bc510f4fa85
CristianBuca <buca.cristian@hotmail.com>
2022-04-27 14:36:37 +0100

    Add(blog-test_forms): test_blog_post_form_no_title

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -10,4 +10,16 @@ class TestBlogPostForm(TestCase):
     """
     Defines the forms unit tests for the blog app
     """
-    
+
+    def test_blog_post_form_no_title(self):
+        """
+        Tests if correct error is displayed to the user when
+        trying to submit form with empty title field
+        Tests if form fails validation
+        """
+        form = BlogPostForm({'title': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('title', form.errors.keys())
+        self.assertEqual(
+            form.errors['title'][0], 'This field is required.'
+        )
