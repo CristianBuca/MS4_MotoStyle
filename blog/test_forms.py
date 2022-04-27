afc2ba5d05a88543bb5dd2c2bc1f8446363bc760
CristianBuca <buca.cristian@hotmail.com>
2022-04-27 14:03:04 +0100

    Start(blog-test_forms): form unit tests

---
+++ blog/test_forms.py
@@ -0,0 +1,13 @@
+# Imports
+# 3rd party:
+from django.test import TestCase
+# Internal
+from .forms import BlogPostForm
+# -----------------------------------------------------------------------------
+
+
+class TestBlogPostForm(TestCase):
+    """
+    Defines the forms unit tests for the blog app
+    """
+    
