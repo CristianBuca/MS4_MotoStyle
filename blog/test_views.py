ed0cec6f5dc125c89acf7184101961f5164f3ca3
CristianBuca <buca.cristian@hotmail.com>
2022-04-27 15:15:18 +0100

    Add(blog-test_views): test_blog

--- blog/test_views.py
+++ blog/test_views.py
@@ -10,4 +10,11 @@ class TestBlogViews(TestCase):
     """
     Defines the views unit tests for the blog app
     """
-    
+    def test_blog(self):
+        """
+        Tests if blog page is accessible with status code of 200
+        Tests if blog view renders blog.html
+        """
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/blog.html')
