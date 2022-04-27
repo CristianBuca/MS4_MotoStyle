f44f32ac552395d162e7e6eb54a6a8a786d2319e
CristianBuca <buca.cristian@hotmail.com>
2022-04-27 15:07:30 +0100

    Add(blog-test_models): test_comment_str

--- blog/test_models.py
+++ blog/test_models.py
@@ -28,3 +28,25 @@ class TestBlogModels(TestCase):
             owner=user,
         )
         self.assertEqual(blog_post.__str__(), blog_post.title)
+
+    def test_comment_str(self):
+        """
+        Tests if Comment model str method returns correct string
+        """
+        user = User.objects.create_user(
+            username='unit_test_user', password='unit_test_pass'
+        )
+        self.client.login(
+            username='unit_test_user', password='unit_test_pass'
+        )
+        blog_post = BlogPost.objects.create(
+            title='Test Blog Post',
+            content='Test Blog Post content',
+            owner=user,
+        )
+        comment = Comment.objects.create(
+            comment_body='Blog Post Comment',
+            posted_by=user,
+            blog_article=blog_post,
+        )
+        self.assertEqual(comment.__str__(), comment.comment_body)
