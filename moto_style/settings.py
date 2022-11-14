d18f30426666333147aa5498ce0c7e813330ebf4
CristianB <buca.cristian@hotmail.com>
2022-11-14 19:42:42 +0000

    Add(build.sh): Render deploy settings

--- moto_style/settings.py
+++ moto_style/settings.py
@@ -35,6 +35,11 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ms4-motostyle.herokuapp.com', '127.0.0.1']
 
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
